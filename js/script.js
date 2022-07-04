77ecd7d364d44f84687102d71111efec14931fc1
Nutelinha <matheus.rlima9@aluno.senai.br>
2022-07-04 08:54:34 -0300

    Contador funcionando, porém, não incrementados, pois ainda não conseguir juntar com o sistema

---
+++ js/script.js
@@ -0,0 +1,30 @@
+function startTime(duration, display) {
+
+    var timer = duration, minutes, seconds;
+
+    setInterval(function() {
+
+        minutes = parseInt(timer / 60 , 10);
+        seconds = parseInt(timer % 60 , 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if(--timer <0){
+            timer = duration;
+        }
+
+    }, 1000)
+}
+
+
+window.onload =  function(){
+
+            var duration = 60*1; //Conversão para segundos
+            var display = document.querySelector("#button"); //Elemento para exibir o timer
+           
+            startTime(duration, display); //Inicia a função
+
+}
